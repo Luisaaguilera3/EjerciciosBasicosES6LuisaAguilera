1681261223f2116f3e61923260e54f6bdc63963c
Luisaaguilera3 <laguileraviasus@gmail.com>
2024-10-02 18:12:23 +0200

    ultimos cambios

--- EJERCICIOS/ejercicio6.js
+++ EJERCICIOS/ejercicio6.js
@@ -1,12 +1,20 @@
 /* 6.1 Dado el siguiente array, usa .find() para econtrar el número 100. */
 const numbers = [32, 21, 63, 95, 100, 67, 43];
 
+const findNumer = numbers.find (number => number === 100);
+console.log(findNumer);
+
+
 /* 6.2 Dado el siguiente array, usa .find() para econtrar la pelicula del año 2010. */
 const movies = [
 	{title: 'Madagascar', stars: 4.5, date: 2015},
 	{title: 'Origen', stars: 5, date: 2010},
 	{title: 'Your Name', stars: 5, date: 2016}
 ];
+
+const movie2010 = movies.find(movie => movie.date === 2010);
+console.log(movie2010);
+
 
 /* 6.3 Dado el siguiente javascript, usa .find() para econtrar el alien de nombre 'Cucushumushu' y la mutación 'Porompompero'. Una vez que los encuentres, usa 
 spread operator para fusionarlos teniendo en cuenta que el objeto de la mutación lo queremos meter en la propiedad .mutation del objeto fusionado. */
@@ -21,3 +29,10 @@ const mutations = [
 	{name: 'Fly me to the moon', description: 'Permite volar, solo y exclusivamente a la luna'},
 	{name: 'Andando que es gerundio', description: 'Invoca a un señor mayor como Personal Trainer'},
 ];
+
+const alien = aliens.find (alien => alien.name === 'Cucushumushu');
+const mutation = mutations.find (mutation => mutation.name === 'Porompompero');
+const fusionAlien = {
+	... alien, mutatition: {...mutation}
+};
+console.log(fusionAlien);
